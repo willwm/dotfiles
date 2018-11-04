0ba06ba6e7458e0ee02697c6e7b8f98642d98229
Will Wolff-Myren <willwm@gmail.com>
2018-11-04 13:09:45 -0800

    Replace stale scripts with nvm.sh and zshvimconf.sh
    Related Gists:
    
    * https://gist.github.com/willwm/9ccca11ae94963265704d440400d623e
    * https://gist.github.com/willwm/0942e821185ce6f99901516ed6d2164f

---
+++ nvm.sh
@@ -0,0 +1,11 @@
+# Install nvm:
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# Configure nvm shell context:
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install Node.js via nvm:
+nvm install node
+nvm use node
