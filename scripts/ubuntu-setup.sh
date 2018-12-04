800df1db761f5b20a844b6d3294bd320d64fca75
Will Wolff-Myren <willwm@gmail.com>
2018-12-03 22:07:37 -0800

    Update ubuntu-setup.sh to include Ukuu PPA

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -3,3 +3,8 @@
 sudo apt update && sudo apt full-upgrade
 
 sudo apt install git git-lfs vim zsh htop mc powertop thermald pandoc
+
+# https://github.com/teejee2008/ukuu
+sudo apt-add-repository -y ppa:teejee2008/ppa
+sudo apt-get update
+sudo apt-get install ukuu
