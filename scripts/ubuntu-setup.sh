e29a427bbba4d9c70eb5cee1ea97fadc28f1796f
Will Wolff-Myren <willwm@gmail.com>
2019-01-10 17:09:54 -0800

    Update ubuntu-setup.sh
    Swap apt-get references with apt; add build-essential curl wget to initial install command

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -2,9 +2,8 @@
 
 sudo apt update && sudo apt full-upgrade
 
-sudo apt install git git-lfs vim zsh htop mc powertop thermald pandoc
+sudo apt install git git-lfs vim zsh htop mc powertop thermald pandoc build-essential curl wget
 
 # https://github.com/teejee2008/ukuu
 sudo apt-add-repository -y ppa:teejee2008/ppa
-sudo apt-get update
-sudo apt-get install ukuu
+sudo apt update && sudo apt install ukuu
