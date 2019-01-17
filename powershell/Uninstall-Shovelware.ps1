e29d4e17f4347affa1646978512123d22a068ee5
Will Wolff-Myren <willwm@gmail.com>
2019-01-16 17:13:51 -0800

    Replace (some) redundant code with functions

--- powershell/Uninstall-Shovelware.ps1
+++ powershell/Uninstall-Shovelware.ps1
@@ -1,83 +1,57 @@
 #--- Uninstall unecessary applications that come with Windows out of the box ---
 
-# 3D Builder
-Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+# --- Functions --- #
+function Get-InstalledAppxPackages()
+{
+    Get-AppxPackage | Sort-Object | ForEach-Object { $_.Name }
+}
 
-# Autodesk
-Get-AppxPackage *Autodesk* | Remove-AppxPackage
+function Uninstall-AppxPackage($packageName) 
+{
+    Write-Host "Uninstall $packageName";
+    Get-AppxPackage $packageName | Remove-AppxPackage;
+}
 
-# Bing Weather, News, Sports, and Finance (Money):
-Get-AppxPackage Microsoft.BingFinance | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingSports | Remove-AppxPackage
-Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
+Set-Alias -Name lsappx -Value Get-InstalledAppxPackages;
+Set-Alias -Name unappx -Value Uninstall-AppxPackage;
 
-# BubbleWitch
-Get-AppxPackage *BubbleWitch* | Remove-AppxPackage
+# --- Commands --- #
 
-# Candy Crush
-Get-AppxPackage king.com.CandyCrush* | Remove-AppxPackage
+# Save a list of currently installed packages to text file...
 
-# Comms Phone
-Get-AppxPackage Microsoft.CommsPhone | Remove-AppxPackage
+lsappx > OriginalAppxPackages.txt;
 
-# Dropbox
-Get-AppxPackage *Dropbox* | Remove-AppxPackage
+# Begin uninstalling...
 
-# Facebook
-Get-AppxPackage *Facebook* | Remove-AppxPackage
-
-# Feedback Hub
-Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
-
-# Get Started
-Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
-
-# Keeper
-Get-AppxPackage *Keeper* | Remove-AppxPackage
-
-# Mail & Calendar
-Get-AppxPackage microsoft.windowscommunicationsapps | Remove-AppxPackage
-
-# Maps
-Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
-
-# March of Empires
-Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
-
-# Messaging
-Get-AppxPackage Microsoft.Messaging | Remove-AppxPackage
-
-# Netflix
-Get-AppxPackage *Netflix* | Remove-AppxPackage
-
-# Office Hub
-Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
-
-# One Connect
-Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
-
-# OneNote
-Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
-
-# People
-Get-AppxPackage Microsoft.People | Remove-AppxPackage
-
-# Phone
-Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage
-
-# Skype (Metro version)
-Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
-
-# Sound Recorder
-Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-
-# Sway
-Get-AppxPackage Microsoft.Office.Sway | Remove-AppxPackage
-
-# Twitter
-Get-AppxPackage *Twitter* | Remove-AppxPackage
-
-# Zune Music, Movies & TV
-Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
-Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+unappx *Autodesk*
+unappx *BubbleWitch*
+unappx *Dropbox*
+unappx *Facebook*
+unappx *Keeper*
+unappx *MarchofEmpires*
+unappx *Netflix*
+unappx *Twitter*
+unappx Fitbit.FitbitCoach
+unappx king.com.CandyCrush*
+unappx Microsoft.3DBuilder
+unappx Microsoft.3DViewer
+unappx Microsoft.BingFinance
+unappx Microsoft.BingNews
+unappx Microsoft.BingSports
+unappx Microsoft.BingWeather
+unappx Microsoft.CommsPhone
+unappx Microsoft.Getstarted
+unappx Microsoft.Messaging
+unappx Microsoft.MicrosoftOfficeHub
+unappx Microsoft.Office.OneNote
+unappx Microsoft.Office.Sway
+unappx Microsoft.OneConnect
+unappx Microsoft.People
+unappx Microsoft.SkypeApp
+unappx microsoft.windowscommunicationsapps
+unappx Microsoft.WindowsFeedbackHub
+unappx Microsoft.WindowsMaps
+unappx Microsoft.WindowsPhone
+unappx Microsoft.WindowsSoundRecorder
+unappx NORDCURRENT.COOKINGFEVER
+unappx ThumbmunkeysLtd.PhototasticCollage
