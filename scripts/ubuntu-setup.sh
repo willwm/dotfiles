c9904a68c1021b2bb8f6c7d336c7a3f47e317f82
Will Wolff-Myren <willwm@gmail.com>
2019-01-16 17:14:54 -0800

    Add Typora install script

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -7,3 +7,8 @@ sudo apt install git git-lfs vim zsh htop mc powertop thermald pandoc build-esse
 # https://github.com/teejee2008/ukuu
 sudo apt-add-repository -y ppa:teejee2008/ppa
 sudo apt update && sudo apt install ukuu
+
+# https://typora.io/#linux
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo apt-add-repository -y 'deb https://typora.io/linux ./'
+sudo apt update && sudo apt install typora
