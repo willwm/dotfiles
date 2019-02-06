334d7bf3c4d2cd9a767365f1e616098c4b7c1f9a
Will Wolff-Myren <willwm@gmail.com>
2019-02-05 16:25:09 -0800

    Add "resizeandcenter.sh" from existing Gist: https://gist.github.com/willwm/265942449ad5a341fef5c7e28e84af0a

---
+++ scripts/resizeandcenter.sh
@@ -0,0 +1,11 @@
+# https://forums.linuxmint.com/viewtopic.php?p=1543828#p1543828
+# https://linux.die.net/man/1/wmctrl
+
+# 1800x1500 (Center)
+wmctrl -r :ACTIVE: -e 0,1020,330,1800,1500
+
+# 2560x1600 (Center)
+wmctrl -r :ACTIVE: -e 0,640,280,2560,1600
+
+# 3200x1900 (Center)
+wmctrl -r :ACTIVE: -e 0,320,130,3200,1900
