fadf0333fb4560abc34e2d11d5e2d294e13504e7
Will Wolff-Myren <willwm@gmail.com>
2019-03-05 09:45:18 -0800

    Add comment, fix line endings

--- profiles/profile.ps1
+++ profiles/profile.ps1
@@ -9,9 +9,10 @@ function Get-PathList
   $Env:Path.Split(";");
 }
 
+# https://github.com/dahlbyk/posh-git#using-posh-git
 function Import-PoshGit
 {
-	Import-Module posh-git
+  Import-Module posh-git;
 }
 
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/set-alias
@@ -19,4 +20,4 @@ Set-Alias -Name la -Value Get-ChildItemForce;
 Set-Alias -Name ll -Value Get-ChildItemForce;
 Set-Alias -Name which -Value Get-Command;
 Set-Alias -Name path -Value Get-PathList;
-Set-Alias -Name posh -Value Import-PoshGit
+Set-Alias -Name posh -Value Import-PoshGit;
