ab4068ba29375d08a6feb5f530d820483b83221a
Will Wolff-Myren <willwm@gmail.com>
2019-03-22 14:53:09 -0700

    Update size templates

--- scripts/resizeandcenter.sh
+++ scripts/resizeandcenter.sh
@@ -4,8 +4,8 @@
 # 1800x1500 (Center)
 wmctrl -r :ACTIVE: -e 0,1020,330,1800,1500
 
-# 2560x1600 (Center)
-wmctrl -r :ACTIVE: -e 0,640,280,2560,1600
-
 # 3200x1900 (Center)
-wmctrl -r :ACTIVE: -e 0,320,130,3200,1900
+wmctrl -r :ACTIVE: -e 0,320,160,3200,1900
+
+# 3640x2000 (Center)
+wmctrl -r :ACTIVE: -e 0,100,100,3640,2000
