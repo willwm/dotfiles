b9dc1c45201c18c92257c8f974ba693270d968fb
Will Wolff-Myren <willwm@gmail.com>
2020-02-16 21:04:27 -0800

    Add exclusions for development folders

--- powershell/Add-MpPreferences.ps1
+++ powershell/Add-MpPreferences.ps1
@@ -3,3 +3,9 @@ Add-MpPreference -ExclusionPath "$Env:UserProfile\Code" -Force
 Add-MpPreference -ExclusionPath "$Env:UserProfile\AppData\Local\Yarn" -Force
 Add-MpPreference -ExclusionPath "$Env:UserProfile\AppData\Roaming\npm" -Force
 Add-MpPreference -ExclusionPath "$Env:UserProfile\AppData\Roaming\npm-cache" -Force
+
+Add-MpPreference -ExclusionPath "C:\DevDB_BK" -Force
+Add-MpPreference -ExclusionPath "C:\ElasticsearchData" -Force
+Add-MpPreference -ExclusionPath "C:\RedisData" -Force
+Add-MpPreference -ExclusionPath "C:\SQLData" -Force
+Add-MpPreference -ExclusionPath "C:\inetpub" -Force
