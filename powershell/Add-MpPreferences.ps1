0143de60cfd4869dbff4c27c1148ad4a934c7c57
Will Wolff-Myren <willwm@gmail.com>
2020-03-28 14:05:38 -0700

    Add more folders and processes to exclusion list

--- powershell/Add-MpPreferences.ps1
+++ powershell/Add-MpPreferences.ps1
@@ -1,11 +1,28 @@
 # Exclude folders from Windows Security On-Access Scan
-Add-MpPreference -ExclusionPath "$Env:UserProfile\Code" -Force
-Add-MpPreference -ExclusionPath "$Env:UserProfile\AppData\Local\Yarn" -Force
-Add-MpPreference -ExclusionPath "$Env:UserProfile\AppData\Roaming\npm" -Force
-Add-MpPreference -ExclusionPath "$Env:UserProfile\AppData\Roaming\npm-cache" -Force
+$folders = @(
+    "$Env:UserProfile\Code",
+    "$Env:UserProfile\AppData\Local\Yarn",
+    "$Env:UserProfile\AppData\Roaming\npm",
+    "$Env:UserProfile\AppData\Roaming\npm-cache",
+    "C:\DevDB_BK",
+    "C:\ElasticsearchData",
+    "C:\RedisData",
+    "C:\SQLData",
+    "C:\inetpub",
+    "C:\Windows\System32\inetsrv\config"
+)
+Add-MpPreference -ExclusionPath $folders -Force
 
-Add-MpPreference -ExclusionPath "C:\DevDB_BK" -Force
-Add-MpPreference -ExclusionPath "C:\ElasticsearchData" -Force
-Add-MpPreference -ExclusionPath "C:\RedisData" -Force
-Add-MpPreference -ExclusionPath "C:\SQLData" -Force
-Add-MpPreference -ExclusionPath "C:\inetpub" -Force
+# Exclude processes from Windows Security On-Access Scan
+$processes = @(
+    "C:\Program Files\Docker\Docker\com.docker.service",
+    "C:\Program Files\Docker\Docker\resources\bin\docker.exe",
+    "C:\Program Files\Docker\Docker\resources\com.docker.backend.exe",
+    "C:\Program Files\Docker\Docker\resources\com.docker.proxy.exe",
+    "C:\Program Files\dotnet\dotnet.exe",
+    "C:\Program Files\nodejs\node.exe",
+    "C:\Program Files (x86)\Yarn\bin\yarn",
+    "$Env:UserProfile\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+)    
+Add-MpPreference -ExclusionProcess $processes -Force
+
