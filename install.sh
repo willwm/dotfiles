d7ed434d675aa22d021c6282356bd87362eeb5c1
Will Wolff-Myren <willwm@gmail.com>
2020-09-10 23:43:05 -0700

    Update .gitignore, add .bash_aliases, update .vim goodies

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 # Aliases
-ln -s ~/.dotfiles/.aliases ~/.aliases
-ln -s ~/.dotfiles/.aliases ~/.bash_aliases
+#ln -s ~/.dotfiles/.aliases ~/.aliases
+#ln -s ~/.dotfiles/.aliases ~/.bash_aliases
