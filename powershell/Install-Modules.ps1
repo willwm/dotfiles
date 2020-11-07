72e616ec82db32b1392f12cf5ba3cddf4559cd3b
Will Wolff-Myren <willwm@gmail.com>
2020-11-07 14:39:34 -0800

    Combine Install-Module commands into Install-Modules.ps1

---
+++ powershell/Install-Modules.ps1
@@ -0,0 +1,5 @@
+# https://github.com/dahlbyk/posh-git#installation
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+
+# https://github.com/JanDeDobbeleer/oh-my-posh#oh-my-posh
+PowerShellGet\Install-Module oh-my-posh -Scope CurrentUser -AllowPrerelease
--- powershell/Posh-Git.ps1
+++
@@ -1,3 +0,0 @@
-# https://github.com/dahlbyk/posh-git#installation
-
-PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
