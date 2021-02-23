69c2c8c20c45f99e4e3d5ce4e3f298922fd59121
Will Wolff-Myren <willwm@gmail.com>
2021-02-22 23:20:55 -0800

    Update nvm script version

--- scripts/nvm-yarn.sh
+++ scripts/nvm-yarn.sh
@@ -1,6 +1,6 @@
 # Install nvm:
 mkdir ~/.nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # Configure nvm shell context:
 export NVM_DIR="$HOME/.nvm"
