d9796fdb643fa988568ddf77582b3fd34257e1b7
Will Wolff-Myren <willwm@gmail.com>
2022-10-30 02:32:32 -0700

    Prefix commands with 'sudo' (https://github.com/lukesampson/psutils/blob/master/sudo.ps1)

--- powershell/Repair-WindowsHealth.ps1
+++ powershell/Repair-WindowsHealth.ps1
@@ -1,5 +1,8 @@
+## Requires 'sudo'
+## scoop install sudo
+
 # https://docs.microsoft.com/en-us/troubleshoot/windows-server/deployment/fix-windows-update-errors
-DISM.exe /Online /Cleanup-image /Restorehealth
+sudo DISM.exe /Online /Cleanup-Image /RestoreHealth
 
 # https://support.microsoft.com/en-us/topic/use-the-system-file-checker-tool-to-repair-missing-or-corrupted-system-files-79aa86cb-ca52-166a-92a3-966e85d4094e
-sfc /scannow
+sudo sfc /scannow
