66dd50b9bde438e793d714d852b7346d3a8a22fe
Will Wolff-Myren <willwm@gmail.com>
2023-05-02 21:50:50 -0700

    Add script based on https://askubuntu.com/questions/1309068/zsh-true-color-disabled-on-wsl2

---
+++ scripts/python-rich.sh
@@ -0,0 +1,11 @@
+# Based on (https://askubuntu.com/questions/1309068/zsh-true-color-disabled-on-wsl2)
+
+# View current (COLOR)TERM environment variables
+env | grep TERM
+
+# Run python module `rich` (https://github.com/Textualize/rich#installing) 
+python3 -m rich
+
+
+
+## TODO: Auto-update .bashrc/.zshrc with `export COLORTERM=truecolor` ##
